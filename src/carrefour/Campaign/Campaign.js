3eac5ce3e5a9c74dd15a3bcf43980161236a9915
h.hamzaoui <hicham.hamzaoui@in-tact.fr>
2021-08-12 14:40:53 +0200

    adding new pages

---
+++ src/carrefour/Campaign/Campaign.js
@@ -0,0 +1,18 @@
+import "./assets/styles.scss";
+function Campaign() {
+  return (
+    <div className="go-to-game-container">
+      <div className="go-to-game-content">
+        <div className="game-period"> FROM THE 1ST TO THE 30TH</div>
+        <div className="title">Try to win your basket</div>
+        <div className="price">100€</div>
+        <div className="description">
+          Refunder for the purchase one of the eligible product
+        </div>
+        <div className="play-button">Play</div>
+      </div>
+    </div>
+  );
+}
+
+export default Campaign;
---
+++ src/carrefour/game/Game.js
@@ -0,0 +1,6 @@
+import "./assets/styles.scss";
+function Game() {
+  return <div className="game-container"></div>;
+}
+
+export default Game;
