83fc4418d575c9651faa26c0102d30fda155c1f8
stephane solida <stephane.solida@in-tact.fr>
2021-09-02 13:08:23 +0200

    added form and validation

--- src/carrefour/pages/Refund/RefundIntro.js
+++ src/carrefour/pages/Refund/RefundIntro.js
@@ -1,22 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./assets/styles.scss";
 import SlotMachine from "../../components/Slotmachine/SlotMachine";
-import Button from "../../components/Button/Button";
 import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
+import Checkbox from "../../components/Checkbox/Checkbox";
+import { Link } from "@reach/router";
 
 function RefundIntro() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { isValid, errors },
+  } = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+  });
   const { t } = useTranslation("message");
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  function agree() {
+    setAgreed(!agreed);
+  }
+  const [agreed, setAgreed] = useState(false);
 
   function RefundIntroContent() {
     return (
       <div className="refund-intro-content content">
         <div className="subtitle">{t("refund.intro.title")}</div>
-        <form action="">
-          <input type="text" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            autoComplete="off"
+            {...register("email", {
+              required: true,
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "format incorrect",
+              },
+            })}
+          />
+          <label htmlFor="email">{t("refund.intro.label")}</label>
+          <div className="agree">
+            <Checkbox checkAction={agree} />
+            <div>
+              {t("refund.intro.agree")}
+              <Link to="/game"> {t("landing.conditions")} </Link>{" "}
+              {t("landing.agreeP2")}
+            </div>
+          </div>
+          <div className="button-area">
+            <button disabled={!isValid || !agreed}>
+              {t("refund.intro.btn")}
+            </button>
+          </div>
         </form>
-        <div className="button-area">
-          <Button text={t("refund.intro.btn")} enable to="/" />{" "}
-        </div>
       </div>
     );
   }
