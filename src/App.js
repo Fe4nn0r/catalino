23340d6e7bfa21a89330786a778e8a0ae02b365b
h.hamzaoui <hicham.hamzaoui@in-tact.fr>
2021-10-06 11:42:32 +0200

    manage Google analytics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./shop/pages/Home/Home";
+import { googleAnalyticsTrackingId } from "./shop/resources/config/config.json";
+import { globalHistory } from "@reach/router";
+import ReactGA from "react-ga";
+
 let Shake = require("shake.js");
 
 function App() {
-  //Shaker part
-  window.onload = function () {
-    //create a new instancey of shake.js.
-    let myShakeEvent = new Shake({
-      threshold: 15,
+  useEffect(() => {
+    initializeShaker();
+    initializeGoogleAnalytics();
+  }, []);
+
+  function initializeGoogleAnalytics() {
+    ReactGA.initialize(googleAnalyticsTrackingId);
+    globalHistory.listen(({ location }) => {
+      ReactGA.pageview(window.location.pathname + window.location.search);
+      console.log("Sending the visite", location.pathname + location.search);
     });
-    // start listening to device motion
-    myShakeEvent.start();
-  };
+    //First info collection
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+  function initializeShaker() {
+    //Shaker part
+    window.onload = function () {
+      //create a new instancey of shake.js.
+      let myShakeEvent = new Shake({
+        threshold: 15,
+      });
+      // start listening to device motion
+      myShakeEvent.start();
+    };
+  }
   return (
     <>
       <Home />
